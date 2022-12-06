739597cda0bd1c3031409a04f0c6e06ac280896e
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-06 22:45:18 +0100

    J

---
+++ src/day05ifstatements/IfStatements01.java
@@ -0,0 +1,33 @@
+package day05ifstatements;
+
+import java.util.Scanner;
+
+public class IfStatements01 {
+    public static void main(String[] args) {
+        //EX:kullanicidan alina sayinin tek mi cift mi oldugunu giriniz
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lutfen bir sayi giriniz");
+        int sayi= scan.nextInt();
+        if (sayi%2==0){
+            System.out.println("Cift sayi");
+        }else {
+            System.out.println("Tek sayi");
+        }
+        if (sayi%2!=0){
+            System.out.println("Cift sayi");
+        }
+
+        //EX:Bir sayinin negatif pozitif veya notr oldugunu soyleyen kodu yaziniz
+        System.out.println("bir sayi giriniz");
+        int sayi2=scan.nextInt();
+        if(sayi2>0){
+            System.out.println("Sayi pozitif");
+        } else if (sayi2==0) {
+            System.out.println("Notr");
+        }else {
+            System.out.println("Negatif sayi");
+        }
+    }
+
+}
