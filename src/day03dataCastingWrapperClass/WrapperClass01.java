ef7fc09beb721857320dc4ba6f18958360cbc9ac
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-06 12:25:38 +0100

    Java

---
+++ src/day03dataCastingWrapperClass/WrapperClass01.java
@@ -0,0 +1,40 @@
+package day03dataCastingWrapperClass;
+
+public class WrapperClass01 {
+    public static void main(String[] args) {
+        /*
+        primitive lerde sadece deger olur
+        non-primitive lerde ise degerler ve kullanisli methodlar vardir
+        Primitive lerin icinde de method olusturmak istendigi icin wrapper class olusturulmustur
+              Primitive+Method=Wrapper Class
+
+
+              Primitive         Wrapper
+                 byte            Byte
+                 short           Short
+                 int             Integer
+                 long            Long
+                 float           Float
+                 double          Double
+                 boolean         Boolean
+                 char            Character
+       Class isimleri buyuk harfle baslar ayrica non primitive lerde buyuk harfle baslar
+       wrapper classlar non-primitive dir
+         */
+
+        byte primitive = 12;//burada his method yoktur
+
+
+        Byte wrapperByte = 12;//variable ismini yazip nokta koydugumuzda bir cok method goruruz
+
+
+        //byte data tipinin en buyuk ve en kucuk degerlerini ekrana yazdiriniz
+        System.out.println("Byte min value = "+Byte.MIN_VALUE+"\nByte max value = "+Byte.MAX_VALUE);
+
+        Short minValue=Short.MIN_VALUE;
+        Short maxValue=Short.MAX_VALUE;
+        System.out.println("maxValue = " + maxValue);
+        System.out.println("minValue = " + minValue);
+
+    }
+}
