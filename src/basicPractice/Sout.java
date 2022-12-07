a92f29033a9110d31c5ea146dcd4b410eae1cbfb
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-07 21:23:13 +0100

    Jp1

---
+++ src/basicPractice/Sout.java
@@ -0,0 +1,23 @@
+package basicPractice;
+
+public class Sout {
+    public static void main(String[] args) {
+        System.out.println("T");
+        System.out.println("E");
+        System.out.println("C");
+        System.out.println("H");
+        System.out.println("P");
+        System.out.println("R");
+        System.out.println("O");
+        System.out.println("E");
+        System.out.println("D");
+        System.out.println("U");
+        System.out.println("C");
+        System.out.println("A");
+        System.out.println("T");
+        System.out.println("I");
+        System.out.println("O");
+        System.out.println("N");
+
+    }
+}
