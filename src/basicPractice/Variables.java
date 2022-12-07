9f5ed0a67b8df042fdaea12df0fb30cca672d30d
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-07 21:33:45 +0100

    Jp4

---
+++ src/basicPractice/Variables.java
@@ -0,0 +1,22 @@
+package basicPractice;
+
+public class Variables {
+    public static void main(String[] args) {
+  //Farkli 3 data turunde variable olusturun ve bunlari yazdirin
+        String okulIsmi="Ilknur";
+        System.out.println("okulIsmi = " + okulIsmi);
+
+        char ilkHarf='K';
+        System.out.println("ilkHarf = " + ilkHarf);
+
+        int sayi=20;
+        System.out.println("sayi = " + sayi);
+
+
+        String isim="ilknur";
+        String soyIsim="Yilmaz";
+
+        System.out.println(isim+" "+soyIsim);
+
+    }
+}
