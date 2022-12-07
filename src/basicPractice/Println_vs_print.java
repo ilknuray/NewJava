139a0076a577b51b993bac30f05a6487bb67126b
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-07 21:26:01 +0100

    Jp2

---
+++ src/basicPractice/Println_vs_print.java
@@ -0,0 +1,13 @@
+package basicPractice;
+
+public class Println_vs_print {
+    public static void main(String[] args) {
+        System.out.println("***********println**********");
+        System.out.println("1");
+        System.out.println("2");
+
+        System.out.println("********print*******");
+        System.out.print("1");
+        System.out.print("2");
+    }
+}
