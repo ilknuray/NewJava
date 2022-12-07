770fecc6fe956a1aad83767300685f325cd4ffb8
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-07 21:20:44 +0100

    Jp

---
+++ src/basicPractice/JavaBasicPractice.java
@@ -0,0 +1,25 @@
+package basicPractice;
+
+public class JavaBasicPractice {
+    public static void main(String[] args) {
+        Short min=Short.MIN_VALUE;
+        System.out.println("min = " + min);
+        Short max=Short.MAX_VALUE;
+        System.out.println("max = " + max);
+
+        Integer minInt=Integer.MIN_VALUE;
+        System.out.println("minInt = " + minInt);
+        Integer maxInt=Integer.MAX_VALUE;
+        System.out.println("maxInt = " + maxInt);
+
+        Byte minByte=Byte.MIN_VALUE;
+        System.out.println("minByte = " + minByte);
+        Byte maxByte=Byte.MAX_VALUE;
+        System.out.println("maxByte = " + maxByte);
+
+        Double minDouble=Double.MIN_VALUE;
+        System.out.println("minDouble = " + minDouble);
+        Double maxDouble=Double.MAX_VALUE;
+        System.out.println("maxDouble = " + maxDouble);
+    }
+}
