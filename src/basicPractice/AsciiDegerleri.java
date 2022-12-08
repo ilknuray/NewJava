1e9d04b4f6aa1fd36414b10dc76f17186ca6c7df
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-08 13:42:57 +0100

    Jp8

---
+++ src/basicPractice/AsciiDegerleri.java
@@ -0,0 +1,26 @@
+package basicPractice;
+
+public class AsciiDegerleri {
+    public static void main(String[] args) {
+        //kucuk harfler buyuk harflerden yuksek degere sahip ascii table da
+
+        char a='a';
+        System.out.println("a = " + a);
+        char A='A';
+        System.out.println("A = " + A);
+        System.out.println("a>A = "+(a>A));
+        //karsilastirma islemlerinin sonucu boolean doner
+//herhangi bir char variable in ascii degrini kod yazarak bulunuz
+
+        char c='c';
+        int sayi=0;
+        System.out.println(c+sayi);
+
+
+        //char karakterleri int e atayabiliriz bu sekilde karakterlerin ascii degerlerini bulabilirz
+
+        int karakter='&';
+        System.out.println("karakter = " + karakter);
+
+    }
+}
