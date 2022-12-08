ab4c95bd05294e07facf9e16f015afe1a141b7ec
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-08 15:30:55 +0100

    Jp18

---
+++ src/basicPractice/IfStatements07.java
@@ -0,0 +1,24 @@
+package basicPractice;
+
+import java.util.Scanner;
+
+public class IfStatements07 {
+    public static void main(String[] args) {
+        //kullanicidan iki sayi alin buyuk olani yazdirin
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lutfen iki sayi giriniz");
+        int sayi1 = sc.nextInt();
+        int sayi2 = sc.nextInt();
+
+        if (sayi2 > sayi1) {
+            System.out.println(sayi2);
+        }else
+            System.out.println(sayi1);
+
+
+        Object buyukSayi=sayi1==sayi2 ? "Sayilar esit" :(sayi1>sayi2 ?sayi1:sayi2);
+        System.out.println("buyukSayi = " + buyukSayi);
+
+    }
+}
