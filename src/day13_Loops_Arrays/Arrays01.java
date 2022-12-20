fd9bb2248e0850ee678df85f7e96b4867e01c433
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-20 21:04:36 +0100

    Java

---
+++ src/day13_Loops_Arrays/Arrays01.java
@@ -0,0 +1,18 @@
+package day13_Loops_Arrays;
+
+import java.util.Arrays;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+        /*
+        Ayni data tipinde coklu datayi depolamak icin Java nin olusturdugu yapilar vardir
+        bu yapilardan biriside arrays lerdir
+         */
+
+        int sayilar[]=new int[7];//array olusturma
+        System.out.println(Arrays.toString(sayilar));//Arraysi yazdirmak icin bu sekilde yapmaliyim
+        //Array olusturdugumuzda icine kac tane koyaczgimizi belirtmemiz gerekli.
+        //
+
+    }
+}
