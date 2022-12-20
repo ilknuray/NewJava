85ae69505ff765194e16228e20dca0e36ff3cc19
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-20 21:25:01 +0100

    Java

--- src/day13_Loops_Arrays/Arrays01.java
+++ src/day13_Loops_Arrays/Arrays01.java
@@ -13,6 +13,19 @@ public class Arrays01 {
         System.out.println(Arrays.toString(sayilar));//Arraysi yazdirmak icin bu sekilde yapmaliyim
         //Array olusturdugumuzda icine kac tane koyaczgimizi belirtmemiz gerekli.
         //
+       sayilar[1]=11;
+       System.out.println(Arrays.toString(sayilar));
+       sayilar[0]=12;
+       sayilar[2]=13;
+       sayilar[3]=14;
+       sayilar[4]=15;
+       sayilar[5]=12;
+       sayilar[6]=16;
+        System.out.println(Arrays.toString(sayilar));
+
+        //Arraydaki herhangi bir elemani nasil yazdiririm
+        System.out.println(sayilar[3]);
+        //Arraydeki en kucuk ve en buyuk elemani yazdiriniz
 
     }
 }
