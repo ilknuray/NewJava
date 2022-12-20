db8f4fd9ba983f8abab0a434bba261b3ffe7622e
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-20 20:36:35 +0100

    Java

---
+++ src/day13_Loops_Arrays/DoWhileLoop01.java
@@ -0,0 +1,23 @@
+package day13_Loops_Arrays;
+
+import java.util.Scanner;
+
+public class DoWhileLoop01 {
+    public static void main(String[] args) {
+        //EX:Kullanicidan bir sayi aliniz sayi 100 den kucuk ise ekrana Kazandin yaziniz
+        //degilse Kaybettin yaziniz.Kullanici kazandikca oyun devam etmeli
+
+        Scanner sc = new Scanner(System.in);
+        int sayi=0;
+        do {
+
+
+            System.out.println("Lutfen bir sayi giriniz");
+             sayi = sc.nextInt();
+
+        if(sayi<100){
+            System.out.println("Kazandiniz");
+        }
+    }while (sayi>=100);
+        System.out.println("Kaybettiniz");
+}}
