3d802ffb8353a1908ce2853b282eb9dd476ae831
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-21 16:40:34 +0100

    Java

--- src/day15ArraysMultidimensionelArrays/Arrays01.java
+++ src/day15ArraysMultidimensionelArrays/Arrays01.java
@@ -7,7 +7,9 @@ public class Arrays01 {
         //binary search method=>bir elemanin Arrayde olup olmadigini anlariz
         int arr[]={12,31,43,14};
         int sayi=43;
-        //bumethodu kullanmadan once sort() methodu kullanilmalidir.bu kullanilmazsa yanlis sonuc verebilir
+        //bu methodu kullanmadan once sort() methodu kullanilmalidir.bu kullanilmazsa yanlis sonuc verebilir
+        //aranilan element array icinde varsa o sayinin indexini dondurur
+        //aranilan element array icinde yoksa - verir ve olsaydi hangi sirada olur onu gosterir index e gore degil
 
         Arrays.sort(arr);
         System.out.println(Arrays.binarySearch(arr, sayi));
