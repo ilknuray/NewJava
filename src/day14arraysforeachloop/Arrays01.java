42d2f8d682da73d5889e17747f15597d9a30994c
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-21 12:26:29 +0100

    Java

--- src/day14arraysforeachloop/Arrays01.java
+++ src/day14arraysforeachloop/Arrays01.java
@@ -5,24 +5,33 @@ import java.util.Arrays;
 public class Arrays01 {
     public static void main(String[] args) {
         //Arrayslerin icine sadece primitive data type lar ve reference lar yerlestirilebilir
-        String str[]=new String[3];
-        str[0]="Java";
-        str[1]="did";
-        str[2]="surprise you";
+        String str[] = new String[3];
+        str[0] = "Java";
+        str[1] = "did";
+        str[2] = "surprise you";
         System.out.println(Arrays.toString(str));//bu sekilde yazdirmazsam sadece reference i yazdirir.
         //Java arrayleri yazdirirken aralarina otomatik olarak virgul koyar
 
         //string bir array olusturunuz ve icine tomdan onceki tum elemanlari yazdiriniz
 
-        String arr[]={"Jane","Mark","Christopher","Tom","Ali","Rojda"};
+        String arr[] = {"Jane", "Mark", "Christopher", "Tom", "Ali", "Rojda"};
 
-        for (String w:arr) {
+        for (String w : arr) {
             System.out.println("w = " + w);
             if (w.equals("Tom")) {
                 break;
             }
 
         }
+        //tom ve jane haric tum elemanlari yazdiriniz
 
+        String brr[] = {"Jane", "Mark", "Christopher", "Tom", "Ali", "Rojda"};
+        for (String w : brr) {
+            if (w.equals("Jane") || w.equals("Tom")) {
+                continue;//bosver demektir
+            }
+            System.out.println("w = " + w);
+
+        }
     }
 }
