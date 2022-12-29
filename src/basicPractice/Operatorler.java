3912e5492c91b1e0bf2e0cc2f93859ff37ad37ff
ilknur yilmaz <ilknuray1318@gmail.com>
2022-12-29 15:02:09 +0100

    Java

--- src/basicPractice/Operatorler.java
+++ src/basicPractice/Operatorler.java
@@ -2,6 +2,8 @@ package basicPractice;
 
 public class Operatorler {
     public static void main(String[] args) {
+
+
         //sayinin 10 ile 20 arasinda olmadigini true diyerek donduren bir kod yaziniz
 
         int sayi=8;
