f6aa773ca32ed7cbab3d65bad6d14a03d4980a19
ilknur yilmaz <ilknuray1318@gmail.com>
2023-01-27 14:38:40 +0100

    Java

---
+++ src/day15ArraysMultidimensionelArrays/MDSArrays02.java
@@ -0,0 +1,11 @@
+package day15ArraysMultidimensionelArrays;
+
+import java.util.Arrays;
+
+public class MDSArrays02 {
+    public static void main(String[] args) {
+        //MDS kisa yoldan nasil olusturulur?
+        char arr[][]={{'a','b'},{'C','D','E'},{'?'}};
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
