8c09689a4d32473cfeb1de657391bcf34b44e02e
ilknur yilmaz <ilknuray1318@gmail.com>
2023-01-27 15:21:43 +0100

    Java

--- src/day16MDSarraylists/MDSArrays01.java
+++ src/day16MDSarraylists/MDSArrays01.java
@@ -1,5 +1,7 @@
 package day16MDSarraylists;
 
+import java.util.Arrays;
+
 public class MDSArrays01 {
     public static void main(String[] args) {
         //Bir tane mds array olusturun ve icindeki elemanlarin toplamini veren kodu yazin
@@ -12,5 +14,28 @@ public class MDSArrays01 {
             }
 
         } System.out.println("sum = " + sum);
+        //Array veya collection varsa foreach kullanmak ilk tercihtir,bunlar var ancak index kullanmak zorunda isem
+        //foreach calismaz diger donguleri kullanmak gerektir
+        //EX:MDS arrayi normal arraye ceviren kodu yaziniz
+
+        int brr[][]={{2,5,1},{32,75}};
+        //MDS deki eleman sayisini bulunuz
+        int elemanSayisi=0;
+        for (int[] w:brr) {
+            for (int k:w) {
+                elemanSayisi++;
+            }
+        }
+        System.out.println("elemanSayisi = " + elemanSayisi);
+             int idx=0;
+        int [] yeniArray=new int[elemanSayisi];
+        for (int[] w:brr) {
+            for (int k:w){
+                yeniArray[idx]=k;
+                idx++;
+            }
+        }
+        System.out.println(Arrays.toString(yeniArray));
+
     }
 }
