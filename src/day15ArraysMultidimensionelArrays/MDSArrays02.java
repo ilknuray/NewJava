3e1b42eb159a90cf50ee3f071e9228d1225ec5b1
ilknur yilmaz <ilknuray1318@gmail.com>
2023-01-27 14:51:25 +0100

    Java

--- src/day15ArraysMultidimensionelArrays/MDSArrays02.java
+++ src/day15ArraysMultidimensionelArrays/MDSArrays02.java
@@ -7,5 +7,15 @@ public class MDSArrays02 {
         //MDS kisa yoldan nasil olusturulur?
         char arr[][]={{'a','b'},{'C','D','E'},{'?'}};
         System.out.println(Arrays.deepToString(arr));
+
+        //Bir string mds array icinde "a" olan elemanlari yazdiriniz
+        String brr[][]={{"learn","java","it"},{"is","easy"}};
+        for (String[] w:brr) {
+            for (String k:w) {
+                if (k.contains("a")){
+                    System.out.println(k +" ");
+                }
+            }
+        }
     }
 }
