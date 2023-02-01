e7de0733ae35fac0909d04ef8665b32c3f48ae2c
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-01 12:16:25 +0100

    Java

--- src/day18arraylistpassbyvalue/ArrayList01.java
+++ src/day18arraylistpassbyvalue/ArrayList01.java
@@ -6,21 +6,38 @@ import java.util.List;
 public class ArrayList01 {
     public static void main(String[] args) {
         //EX:1 ir list icideki 7 haric tum elemanlari 2 arttiiniz
-        List<Integer>ages=new ArrayList<>();
+        List<Integer> ages = new ArrayList<>();
         ages.add(12);
         ages.add(7);
         ages.add(21);
         ages.add(78);
         ages.add(9);
 
-        for (Integer w:ages) {
-            if (w==7){
+        for (Integer w : ages) {
+            if (w == 7) {
                 continue;
             }
             //w=w+2;=>Listler boyle update edilmez
-            ages.set(ages.indexOf(w),w+2);
+            ages.set(ages.indexOf(w), w + 2);
 
         }
         System.out.println("ages = " + ages);
+        //EX2:Sise verilen arrayListteki 8 VE 8 den onceki tum elemanlari iki katina cikariniz
+
+        List<Integer> sayilar = new ArrayList<>();
+        sayilar.add(12);
+        sayilar.add(7);
+        sayilar.add(21);
+        sayilar.add(8);
+        sayilar.add(9);
+        System.out.println("sayilar = " + sayilar);
+        for (Integer w : sayilar) {
+            if (w<=8){
+                continue;
+            }
+            sayilar.set(sayilar.indexOf(w),w*2);
+
+        }
+        System.out.println("sayilar sonraki hali= " + sayilar);
     }
 }
