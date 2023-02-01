290acd25dd1cd8ba0538be3e0187fde041589172
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-01 14:07:17 +0100

    Java

--- src/day18arraylistpassbyvalue/Varargs01.java
+++ src/day18arraylistpassbyvalue/Varargs01.java
@@ -5,6 +5,13 @@ public class Varargs01 {
         int a=5;
         int b=6;
         System.out.println(add(2,3));
+        //1)farkli sayilarda ki parametrelerle ilgili islem yapmak isterseniz varrargs kullanmalisiniz
+        //2)varargs arka planda array kullanir bu yuzden arraylerle calisiyor gibi davranabilirsiniz
+        //3)varargs olusturmak icin data type ... varargs isim
+        //4)bir methodta varargs in yaninda baska bir parametre kullanilabilir mi?
+        //kullanilabilir ancak varargs lar en sona yazilmalidir onune baska parametreler konulabilir.ikinci kima yazarsak ulasilamayan kod olur
+        //5)Bir methodta birden fazla varrargs kullanilabilir mi?
+        //kullanilamaz cunku hangisi onde olursa olsun sonrasinda gelen parametreler ulasilamayan parametre olurlar
     }
 
     private static void topla( int a,int b) {
