31bbb4012a199f6a55186aa4970aee426ffeabc5
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-01 15:10:49 +0100

    Java

--- src/day18arraylistpassbyvalue/MethodOverLoading01.java
+++ src/day18arraylistpassbyvalue/MethodOverLoading01.java
@@ -6,7 +6,11 @@ public class MethodOverLoading01 {
         add(4.6,7);
         add(3,5,7);
         add(3,5.5);
-
+//ayni isimli farkli parametreli methodlar uretmeye overloading denir.indexof() buna ornektir
+        //1)Method isimleri ayni olmalidir
+        //2)Parametreler arkli olmalidir
+        //parametre sayisi yada data typleri degistirilebilir,parametrelerin yerleri degistirilerek yapilabilirler ancak data typelari farkli ise
+        //3)Method ismi + parametre=method signature access modifierlari degistirsem bile hata verir java sadece method signatur e bakar.
     }
     public static void add(int a,int b){
         System.out.println(a+b);
