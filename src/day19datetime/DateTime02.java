5f5c0a0959a251ee3359099ddc13985bf25cedb5
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-01 16:07:02 +0100

    Java

--- src/day19datetime/DateTime02.java
+++ src/day19datetime/DateTime02.java
@@ -7,9 +7,18 @@ public class DateTime02 {
     public static void main(String[] args) {
         //EX1:javadan aldiginiz date i ay gun yil olark yaziniz
         LocalDate currrentDate=LocalDate.now();
+        System.out.println("currrentDate = " + currrentDate);
         DateTimeFormatter dtf=DateTimeFormatter.ofPattern("MM/dd/yyyy");
         String formatedDate=dtf.format(currrentDate);
         System.out.println("formatedDate = " + formatedDate);
+
+        DateTimeFormatter dtf2=DateTimeFormatter.ofPattern("MMM/dd/yyyy");
+        String formatedDate2=dtf2.format(currrentDate);
+        System.out.println("formatedDate2 = " + formatedDate2);
+
+        DateTimeFormatter dtf3=DateTimeFormatter.ofPattern("MMMM/dd/yyyy");
+        String formatedDate3=dtf3.format(currrentDate);
+        System.out.println("formatedDate3 = " + formatedDate3);
 
     }
 }
