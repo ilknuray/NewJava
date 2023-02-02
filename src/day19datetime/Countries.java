dee93d981645e22692e0c785c91a98a854ca2d51
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-02 11:48:24 +0100

    Java

---
+++ src/day19datetime/Countries.java
@@ -0,0 +1,6 @@
+package day19datetime;
+
+public enum Countries {
+
+    USA,UK,FRANCE,CANADA,TURKEY,GERMANY,RWANDA
+}
--- src/day19datetime/DateTime03.java
+++ src/day19datetime/DateTime03.java
@@ -17,5 +17,39 @@ public class DateTime03 {
         //EX2:sabit bir tarih olusturunuz
         LocalDate trh=LocalDate.of(2011, Month.DECEMBER,21);
         System.out.println("trh = " + trh);
+
+        /*
+        Javada sabit datalari (ay yil gun isimleri,amerika eyalet isimleri gibi);
+        sabit datalari saklamak ve gerektiginde kullanmak icin sabit dtalar konur ve bunlara "Enum" denir.
+         */
+
+        //EX3:USA ICIN wOOOW YAIN uk ICIN big CANADA icin Cold TURKEY icin Vatan..
+        Countries coutry=Countries.USA;
+
+        switch (coutry){
+            case UK:
+                System.out.println("Big");
+                break;
+            case USA:
+                System.out.println("Wooow");
+                break;
+            case FRANCE:
+                System.out.println("Kruvasan");
+                break;
+            case GERMANY:
+                System.out.println("Araba");
+                break;
+            case TURKEY:
+                System.out.println("Vatan");
+                break;
+            case CANADA:
+                System.out.println("cold");
+                break;
+            case RWANDA:
+                System.out.println("cay");
+                break;
+            default:
+                System.out.println("Tanimlanmamis ulke");
+        }
     }
 }
