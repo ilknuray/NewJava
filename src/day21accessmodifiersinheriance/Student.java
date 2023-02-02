468c7f883a58d3c83f83a021246faaf1498c1fb6
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-02 20:31:53 +0100

    Java

---
+++ src/day21accessmodifiersinheriance/Student.java
@@ -0,0 +1,18 @@
+package day21accessmodifiersinheriance;
+
+public class Student {
+    /*
+    Access MODIFIER
+    1)public=>herkes
+    2)protected=>child classlar
+    3)default=>sadece o package
+    4)private=>sadece o class
+     */
+    public String stdName="TomHanks";
+    protected String adress="Miami";
+    String email="tomhanks@gmail.com";//defult yazmayiz zaten otomatik olarak default degeri verir
+    private String stdId="20206517004";
+
+
+
+}
