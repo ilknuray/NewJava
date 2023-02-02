ea87266a989db3cd005669376925bb8cac82b218
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-02 14:38:43 +0100

    Java

---
+++ src/day20stringbuilderstringbuffer/StringBuffer01.java
@@ -0,0 +1,19 @@
+package day20stringbuilderstringbuffer;
+
+public class StringBuffer01 {
+    public static void main(String[] args) {
+        /*
+        1)StringBuffeer java da bir classtir ve string olusturmaya yarar
+        2)StringBuffer class String builder class gibi mutable dir
+
+        StringBuffer ile StringBuildeer in farki nedir?
+        1)Stringbuffer theared-safe dir
+        2)Stringbuffer syncronized tir
+
+        senaryolar
+        1)Degistirilemez data icin String class kullanilir
+        2)Degistirilebilir ama multi theared gerekmeyen durumlar icin String builder kullanilir
+        3)Degistirilebilir ve multi thread geren durumlar icin Stringbuffer kullanilir
+         */
+    }
+}
