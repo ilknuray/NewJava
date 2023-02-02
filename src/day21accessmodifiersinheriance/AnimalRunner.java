3e90e688dd644bb9f7c586951e9f8fe4e4ebdd4c
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-02 21:43:18 +0100

    Java

--- src/day21accessmodifiersinheriance/AnimalRunner.java
+++ src/day21accessmodifiersinheriance/AnimalRunner.java
@@ -2,10 +2,22 @@ package day21accessmodifiersinheriance;
 
 public class AnimalRunner {
     public static void main(String[] args) {
-        Cat cat=new Cat();
+        Cat cat = new Cat();
         cat.eat();
         cat.miyav();
         cat.drink();
+        System.out.println("");
+
+        Dog dog = new Dog();
+        dog.havla();
+        dog.drink();
+        dog.eat();
+        System.out.println("");
+
+        Bird bird = new Bird();
+        bird.oter();
+        bird.eat();
+        bird.drink();
 
 
     }
