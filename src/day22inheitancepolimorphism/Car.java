fdd8b5f8e754783b570a39c9ac6a2eb8e1165499
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-03 19:19:20 +0100

    Java

--- src/day22inheitancepolimorphism/Car.java
+++ src/day22inheitancepolimorphism/Car.java
@@ -1,5 +1,15 @@
 package day22inheitancepolimorphism;
 
 public class Car extends Vehicule{
+    public String color;
+    public int km;
 
+    public Car(){
+
+    }
+    public Car(String color,int km){
+        this.color=color;
+        this.km=km;
+
+    }
 }
--- src/day22inheitancepolimorphism/Honda.java
+++ src/day22inheitancepolimorphism/Honda.java
@@ -1,5 +1,15 @@
 package day22inheitancepolimorphism;
 
 public class Honda extends Car {
+    public String color;
+
+    public Honda() {
+
+    }
+
+    public Honda(String color) {
+
+        this.color = color;
+    }
 
 }
