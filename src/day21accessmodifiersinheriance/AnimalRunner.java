ffe308dc28c1827d8817155aa06d7c5511d8906d
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-03 11:53:23 +0100

    Java

--- src/day21accessmodifiersinheriance/AnimalRunner.java
+++ src/day21accessmodifiersinheriance/AnimalRunner.java
@@ -18,6 +18,21 @@ public class AnimalRunner {
         bird.oter();
         bird.eat();
         bird.drink();
+        /*
+        1)
+        *inheritance sayesinde kodtekrarindan kurtuluruz
+        *maintenance(kod tamiori) kolay olur
+        *Child classlari daha atomoik bir yapiya getiririz
+        2)
+        Bir classi baska bir classin child classi yapmak icin extends keyword kullanilir.ilk yazilan child class ikinci yazilan parent class olur
+        3)
+        Child class objectleri parent classtan method ve variablelari kullanabilir
+        4)
+        Parent class objectleri child classtan method ve variable kullanamazlar
+        5)
+        object class her classin parentidir
+        Javada object class haric heer classin parenti vardir
+         */
 
 
     }
