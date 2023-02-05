67f170aecda23bbba48b0360d5fdb746cbe53f7b
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-05 20:34:41 +0100

    Java

--- src/day23inheritancepolymorphism/Math.java
+++ src/day23inheritancepolymorphism/Math.java
@@ -1,7 +1,15 @@
 package day23inheritancepolymorphism;
 
-public class Math {
+public class Math extends Courses {
     public void practise(){
         System.out.println("Solve questions");
+    }
+    public Math(){
+        super("second");
+        System.out.println("Constructor 1");
+    }
+    public Math(int a){
+        this();//ayni classin icinde diger costructori kullan
+        System.out.println("Constructor 2");
     }
 }
