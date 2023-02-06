acb188eee9efa35bc3cf8cb50f9081825b90ed84
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-06 20:44:11 +0100

    Java

---
+++ src/day25exceptions/Exeption01.java
@@ -0,0 +1,12 @@
+package day25exceptions;
+
+public class Exeption01 {
+    /*Beklenmeyen istisnai durumlarla karsilasilmasina exception denir
+    Java da program calistirildiginda farkli sorunlar olabilir
+    *programcinin kod hatasi
+    *kullanicinin uygun girisler yapmmasi
+    *internet baglantisinin kesilmesi
+    java cozemzdigi sorunla karsilasinca calismayi durdurur ve exception atar
+     */
+
+}
