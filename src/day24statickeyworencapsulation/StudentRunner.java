10ba3ed4f619d4ca015a44eafe1f6b01ee5f9ead
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-06 20:17:17 +0100

    Java

--- src/day24statickeyworencapsulation/StudentRunner.java
+++ src/day24statickeyworencapsulation/StudentRunner.java
@@ -7,6 +7,11 @@ public class StudentRunner {
         std1.setId("AB");
         System.out.println(std1.getId());
         //set sadece degistirir ancak okuma islemi yani gorebilmek icin get methodu kullanmaliyiz
+        //butun degisimler objct ustundedir class ta degil
+
+        //NOTEEE:encapsulation sayesinde bir islem icin 4 farkli deger kulanmam gerekiyorsa bu degerler
+        // icin 4 farkli object uretmeden set methodu ile donusturerek kullanabiiriz.bir object i 1000 object olarak kullanabiliriz
+
 
     }
 }
