0fdfafc8aad84eb6fd4c68919a0815b43431f676
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-06 20:50:41 +0100

    Java

--- src/day25exceptions/Exeption01.java
+++ src/day25exceptions/Exeption01.java
@@ -8,5 +8,13 @@ public class Exeption01 {
     *internet baglantisinin kesilmesi
     java cozemzdigi sorunla karsilasinca calismayi durdurur ve exception atar
      */
+    public static void main(String[] args) {
+        divide(6,2);
+        divide(0,2);
+        divide(6,0);
+    }
+        public static void divide(int a,int b){
+            System.out.println(a/b);
+        }
 
 }
