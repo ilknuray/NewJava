bfd35b01310b57e797b25988ec6ac4e149452636
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-07 13:50:35 +0100

    Java

--- src/day26exeptions/Exceptions01.java
+++ src/day26exeptions/Exceptions01.java
@@ -1,5 +1,26 @@
 package day26exeptions;
 
 public class Exceptions01 {
+    public static void main(String[] args) {
+        String str = "123";
+        int result = convertStringToInteger(str);
+        System.out.println(result + 5);
 
+        String st = "1a3";
+        int sonuc = convertStringToInteger(st);
+        System.out.println(sonuc);
+
+    }
+
+    public static int convertStringToInteger(String str) {
+        int i=0;
+        try {
+            i= Integer.valueOf(str);
+        } catch (NumberFormatException e) {
+            System.out.println("rakam yazmalisin");
+        }
+
+
+        return i;
+    }
 }
