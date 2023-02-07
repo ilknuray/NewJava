86f3599399b7760ee399db7758d45f4098997545
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-07 14:16:47 +0100

    Java

--- src/day26exeptions/Exception02.java
+++ src/day26exeptions/Exception02.java
@@ -7,15 +7,15 @@ public class Exception02 {
         System.out.println(getCharFromString(str, 6));//StringIndexOutOfBoundsException==>Bir stringde var olmayan index i kullandigimizda aldigimiz hatadir
 
     }
-    //bir stringin istedigimiz ir karakterini bize veren method
+    //bir stringin istedigimiz bir karakterini bize veren method
     public static char getCharFromString(String str,int idx){
-        int i=0;
+        char i=' ';
         try {
             i=str.charAt(idx);
         }catch (StringIndexOutOfBoundsException e){
             System.out.println("Stringin indexi asildi");
 
         }
-      return (char) i;
+      return i;
     }
 }
