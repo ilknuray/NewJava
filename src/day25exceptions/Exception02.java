1658945a21d8dc62cb58a627e405b6a6f584ecd1
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-07 12:45:01 +0100

    Java

---
+++ src/day25exceptions/Exception02.java
@@ -0,0 +1,19 @@
+package day25exceptions;
+
+public class Exception02 {
+    public static void main(String[] args) {
+        //bir strinng arrayden index girerek eleman elde edebilmek icin bir method olusturun
+        String arr[]={"Ali","Veli","49","50"};
+        getElementFromArray(arr,2);
+        getElementFromArray(arr,0);
+        getElementFromArray(arr,5);
+    }
+    public static void getElementFromArray(String arr[],int idx){
+        try {
+            System.out.println(arr[idx]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Index asildi ==>"+e.getMessage());
+        }
+
+    }
+}
