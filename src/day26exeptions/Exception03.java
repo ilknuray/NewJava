d0bcb21f24d58e374a6859cb466f5150ca08d273
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-07 21:21:03 +0100

    Java

--- src/day26exeptions/Exception03.java
+++ src/day26exeptions/Exception03.java
@@ -2,36 +2,55 @@ package day26exeptions;
 
 public class Exception03 {
     public static void main(String[] args) {
-        String str="123";
+        String str = "123";
         System.out.println(karakterSayisi(str));
 
-        String b=null;
+        String b = null;
         System.out.println(karakterSayisi(b));//NullPointerException++>null string ile length() kullanirsak
 
-        String a="";
+        String a = "";
         System.out.println(karakterSayisi(a));//NumberFormatException==>String ifade yerine baska birsey kulanirsam
 
     }
     //stringdeki karakter sayisini bulunuz,int e ceviriniz,integeri karakter sayisina bolunuz
 
-    public static double karakterSayisi(String str){
-        int lenght=0;
+    public static double karakterSayisi(String str) {
+        int lenght = 0;
 
-        int s=0;
+        int s = 0;
 
-        double sonuc=0;
+        double sonuc = 0;
 
         try {
-            lenght=str.length();
-            s=Integer.valueOf(str);
-            sonuc=s/lenght;
-        }catch (NullPointerException e){
-            System.out.println(e.getMessage());
-        }catch (NumberFormatException e){
-            System.out.println(e.getMessage());
+            lenght = str.length();
+            s = Integer.valueOf(str);
+            sonuc = s / lenght;
+        } catch (NullPointerException e) {
+            System.out.println("1 "+e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("2 "+e.getMessage());
         }
 
         return sonuc;
 
+    }
+
+    public static double karakterSayisi2(String str) {
+        int lenght = 0;
+
+        int s = 0;
+
+        double sonuc = 0;
+
+        try {
+            lenght = str.length();
+            s = Integer.valueOf(str);
+            sonuc = s / lenght;
+        } catch (Exception e) {
+            System.out.println("3 "+e.getMessage());
+
+
+        }
+        return sonuc;
     }
 }
