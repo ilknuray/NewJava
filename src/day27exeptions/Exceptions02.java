aba94522e80479151cfbc28f90beda61c9dd1058
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 11:46:04 +0100

    Java

--- src/day27exeptions/Exceptions02.java
+++ src/day27exeptions/Exceptions02.java
@@ -2,19 +2,21 @@ package day27exeptions;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Exceptions02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        readTheTextFromTheFile();
 
     }
 
     //Bir text file daki texti okuyan kodu yaziniz
-    public static void readTheTextFromTheFile() throws FileNotFoundException {
+    public static void readTheTextFromTheFile() throws IOException {
 
         FileInputStream fis = new FileInputStream("src/day27exeptions/File1.txt");//adres yanlis yada dosya bos olabilir diye java bizi ikaz eder CTE verir.
         int k=0;
         while ((k= fis.read())!=-1){
-            System.out.println((char)k);
+            System.out.print((char)k);
         }
 
     }
