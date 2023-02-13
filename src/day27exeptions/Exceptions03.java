09538bc887dd9ed09e2fe7a42fa3d39451ad0f61
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 15:27:16 +0100

    Java

--- src/day27exeptions/Exceptions03.java
+++ src/day27exeptions/Exceptions03.java
@@ -1,7 +1,17 @@
 package day27exeptions;
 
 public class Exceptions03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalGradeException {
 
+        IllegalGradeException ige=new IllegalGradeException("abc");
+notlariYazdir(50);
+    }
+    //ogrenci notlarini yazdiran bir method oluisturun
+    public static void notlariYazdir(double not) throws IllegalGradeException {
+        if (not<0 || not>100){
+            throw new IllegalGradeException("Not 0 dan kucuk 100 den buyuk olamaz");
+        }else {
+            System.out.println(not);
+        }
     }
 }
--- src/day27exeptions/IllegalGradeException.java
+++ src/day27exeptions/IllegalGradeException.java
@@ -1,5 +1,8 @@
 package day27exeptions;
 
 public class IllegalGradeException extends Exception{
-
+    //eger Exception classa extends ederseniz sizin classiniz CTE olur
+public IllegalGradeException(String message){
+    super(message);
+}
 }
