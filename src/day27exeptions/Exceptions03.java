f2f07287c27ee4f27483f25ce7325997d0525a66
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 15:36:00 +0100

    Java

--- src/day27exeptions/Exceptions03.java
+++ src/day27exeptions/Exceptions03.java
@@ -3,15 +3,26 @@ package day27exeptions;
 public class Exceptions03 {
     public static void main(String[] args) throws IllegalGradeException {
 
-        IllegalGradeException ige=new IllegalGradeException("abc");
-notlariYazdir(50);
+        IllegalGradeException ige = new IllegalGradeException("abc");
+        notlariYazdir(50);
+        ilkHarfBuyukOlmali("Ilknurr");
     }
+
     //ogrenci notlarini yazdiran bir method oluisturun
     public static void notlariYazdir(double not) throws IllegalGradeException {
-        if (not<0 || not>100){
+        if (not < 0 || not > 100) {
             throw new IllegalGradeException("Not 0 dan kucuk 100 den buyuk olamaz");
-        }else {
+        } else {
             System.out.println(not);
+        }
+    }
+    //Verilen ismin ilk harfinin buyuk harf olmamasi durumunda exception atan kodu yaziniz
+
+    public static boolean ilkHarfBuyukOlmali(String name){
+        if (name.charAt(0)>='A' && name.charAt(0)<='Z'){
+            return true;
+        }else {
+            throw new IllegalNameException("Isimler buyk harfle baslamalidir");
         }
     }
 }
---
+++ src/day27exeptions/IllegalNameException.java
@@ -0,0 +1,8 @@
+package day27exeptions;
+
+public class IllegalNameException extends RuntimeException{
+
+    public IllegalNameException(String mesage) {
+        super(mesage);
+    }
+}
