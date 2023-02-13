2ee48b312bbf4562472b2cf024693387209c8d72
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 11:51:43 +0100

    Java

--- src/day27exeptions/Exceptions02.java
+++ src/day27exeptions/Exceptions02.java
@@ -19,5 +19,17 @@ public class Exceptions02 {
             System.out.print((char)k);
         }
 
+        /*
+        1)FileInputStream fis = new FileInputStream("src/day27exeptions/File1.txt");
+        new den sonraki kisim hata verir.cunku biz java ya verilen adrese git dedik java iki endiseye kapilidi.
+        *Ya adres yanlissa
+        **Ya bu dosya sikinmisse
+        Biz method isminden sonre "throws FilNotFoundException " yazarak java ya bu iki endise duydugun durum olursa excption at dedik
+
+        2) while ((k= fis.read())!=-1){ yazdigimizda "read()" methodu hata verir.cunku biz java ya dosyadaki karakterleri oku dedik.
+        Java bir endiseye kapildi
+        *Ya okumasi gerekne karakterler javanin bilmedigi karakterlerse
+         */
+
     }
 }
