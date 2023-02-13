e5839065681e5bcdc4daf8aa991d92254a8579bc
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 16:29:43 +0100

    Java

--- src/day28abstraction/ReadFileLineByLine.java
+++ src/day28abstraction/ReadFileLineByLine.java
@@ -6,6 +6,13 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadFileLineByLine {
+    /*
+    throws ile throws araindaki farklar nelerdir?
+    1)throw methodun bodysinin icinde kullanilir thows ise methodun isminden sonra kullaniilir
+    2)throw dan sonra object olusturulur,throws tan sonra ise exception classin ismi yazilir
+    3)throw methodun icinde istedigimiz yerde exception uretmek  icin kullanilir,throws ise var olan checked exceptioni atmak icin kullanilir
+    4)throw dan sadece bir tane exception olabilir,throwsdan sonra birden fazla exception olabilir
+     */
     public static void main(String[] args) {
         readFile();
 
