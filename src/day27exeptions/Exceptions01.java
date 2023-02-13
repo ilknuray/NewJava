b35e55f96cbc23da847f646cf4187cea549be22b
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 11:08:26 +0100

    Java

--- src/day27exeptions/Exceptions01.java
+++ src/day27exeptions/Exceptions01.java
@@ -2,6 +2,27 @@ package day27exeptions;
 
 public class Exceptions01 {
     public static void main(String[] args) {
+/*
+coding te her sart altinda yapilmasi gerekne seyler vardir.APP==>DB app ler database ler ile etkilesim halindedir.
+AWS-->amazon web service database ile isim bitince connectioni kesmem gereklidir yoksa masraf cok olur
 
+ */
+        double sonuc = kacKati("Java", "xy");
+        System.out.println(sonuc);
+    }
+    //Verilen iki stringten birinin karakter sayisinin digerinin kac kati olduhgunu veren method olusturunuz
+
+    public static double kacKati(String s, String t) {
+        double result=0;
+        try {
+            result=s.length() / t.length();
+        }catch (NullPointerException e){
+            System.out.println("length methodu null ile kullailamaz");
+
+        }catch (ArithmeticException e){
+            System.out.println("herhangi bir sayi sifir ile bolunemez");
+
+        }
+       return result;
     }
 }
