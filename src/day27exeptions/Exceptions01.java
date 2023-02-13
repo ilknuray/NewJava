537061654684d29ecc0d546242eb455ff375c7d2
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 11:18:40 +0100

    Java

--- src/day27exeptions/Exceptions01.java
+++ src/day27exeptions/Exceptions01.java
@@ -13,18 +13,31 @@ AWS-->amazon web service database ile isim bitince connectioni kesmem gereklidir
     //Verilen iki stringten birinin karakter sayisinin digerinin kac kati olduhgunu veren method olusturunuz
 
     public static double kacKati(String s, String t) {
-        double result=0;
+        double result = 0;
         try {
-            result=s.length() / t.length();
-        }catch (NullPointerException e){
+            result = s.length() / t.length();
+        } catch (NullPointerException e) {
             System.out.println("length methodu null ile kullailamaz");
 
-        }catch (ArithmeticException e){
+        } catch (ArithmeticException e) {
             System.out.println("herhangi bir sayi sifir ile bolunemez");
 
-        }finally {
+        } finally {
             System.out.println("Dtabase ile connection kesildi");
         }
-       return result;
+        return result;
     }
+    /*
+    final,finally,finilaze aciklayiniz
+    finally:bir kod blogudur.try catch veya sadece try ile kullanilir.
+            finally kod block icine yazilan kodlar her halukarda calisir
+            finally en sonda kullanilir
+            Mesela database ile connectioni kesmek herhalukarda yailmasi gereken bir seydir bunu finally ile yapabiliriz
+    finalize:java finalize edilmemeis hicbir seyi silmez
+    java bunu kendisi yapar
+    finalize bir methodtur bu method java tarafindan datalar imha edilmeden once cagrilir bu method datalari imha edilecek hjale getirir
+    ve daha sonra garbage collector bu datalari imha eder.
+    once finalize eder sonra garbage collector devreye girer
+    note:finalize methodunu java developerlarda cagirabilir ama java kendi bildigini yapar
+     */
 }
