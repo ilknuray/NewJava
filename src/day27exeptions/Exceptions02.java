d56d02c3978f1845227e3c7887eea9e6c707e4a9
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 11:35:50 +0100

    Java

--- src/day27exeptions/Exceptions02.java
+++ src/day27exeptions/Exceptions02.java
@@ -7,10 +7,12 @@ public class Exceptions02 {
     public static void main(String[] args) {
 
     }
+
     //Bir text file daki texti okuyan kodu yaziniz
-public static void readTheTextFromTheFile() throws FileNotFoundException {
+    public static void readTheTextFromTheFile() throws FileNotFoundException {
 
-    FileInputStream fis=new FileInputStream("src/day27exeptions/File1.txt");
+        FileInputStream fis = new FileInputStream("src/day27exeptions/File1.txt");//adres yanlis yada dosya bos olabilir diye java bizi ikaz eder CTE verir.
 
-}
+
+    }
 }
