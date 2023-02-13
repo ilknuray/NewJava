efcefa2af610044c571dbcaf98d2bf3e54534c1f
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 11:34:48 +0100

    Java

--- src/day27exeptions/Exceptions02.java
+++ src/day27exeptions/Exceptions02.java
@@ -1,9 +1,16 @@
 package day27exeptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Exceptions02 {
     public static void main(String[] args) {
 
     }
     //Bir text file daki texti okuyan kodu yaziniz
+public static void readTheTextFromTheFile() throws FileNotFoundException {
 
+    FileInputStream fis=new FileInputStream("src/day27exeptions/File1.txt");
+
+}
 }
