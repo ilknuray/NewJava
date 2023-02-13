ae417d16535ecde6058d96e7f749233ada2a258f
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-13 16:02:16 +0100

    Java

---
+++ src/day28abstraction/ReadFileLineByLine.java
@@ -0,0 +1,22 @@
+package day28abstraction;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileLineByLine {
+    public static void main(String[] args) {
+
+    }
+    public static void readFile(){
+        try {
+            BufferedReader br=new BufferedReader(new FileReader("src/day27exeptions/File1.txt"));
+           String line= br.readLine();
+        } catch (FileNotFoundException e) {
+            System.out.println("Dosya yolu yanlis yada dosya silinmistir"+e.getMessage());
+        } catch (IOException e) {
+            System.out.println("okunamayacak karakter veya karakterler var"+e.getMessage());
+        }
+    }
+}
