9b2b496b3594200b33988d9a8ea4872861199a99
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 21:23:25 +0100

    Java

--- src/day30collections/LinkedHashSet01.java
+++ src/day30collections/LinkedHashSet01.java
@@ -1,4 +1,19 @@
 package day30collections;
 
+import java.util.LinkedHashSet;
+
 public class LinkedHashSet01 {
+    public static void main(String[] args) {
+        /*
+        linkedhash setler elemanlari ekleme sirasina gore yerlestirirler
+         */
+        LinkedHashSet<String>emails=new LinkedHashSet<>();
+        emails.add("abc@gmail.com");
+        emails.add("def@gmail.com");
+        emails.add("ghi@gmail.com");
+        emails.add("jkl@gmail.com");
+        emails.add("mno@gmail.com");
+        System.out.println("emails = " + emails);
+
+    }
 }
