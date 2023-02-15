6330ca64bc9546642851643da48c71608a23b89f
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 16:43:48 +0100

    Java

--- src/day30collections/Hashset01.java
+++ src/day30collections/Hashset01.java
@@ -1,7 +1,21 @@
 package day30collections;
 
+import java.util.HashSet;
+
 public class Hashset01 {
     public static void main(String[] args) {
-
+    //birbirine benzemeyen data olusturmaya yarar.orneginuniversite ogrenci numarasi gibi bolum kodu+ogrencino+universite kodu+fakulte kodu
+        /*
+        1)Hash bir tkniktir.birbirine benzemeyen kodlar uretir bunlar datayi unique yapar
+         */
+        HashSet<String>emails=new HashSet<>();
+        emails.add("apple");
+        emails.add("mango");
+        emails.add("orange");
+        emails.add("apricot");
+        emails.add("fig");
+        System.out.println("emails = " + emails);
+        emails.add("mango");
+        System.out.println(emails);//ayni elemani eklersek hata vermez ayni elemani olan elmanin ustune yazdigi icin tekra gormeyiz.override yapar
     }
 }
