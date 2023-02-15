e2c5399c4aefadd3e9c6dde25cfb952ba63ce7bc
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 22:08:05 +0100

    Java

--- src/day30collections/PriorityQueue01.java
+++ src/day30collections/PriorityQueue01.java
@@ -6,10 +6,13 @@ public class PriorityQueue01 {
     public static void main(String[] args) {
         PriorityQueue <String>myQueue=new PriorityQueue<>();
         myQueue.add("A");
-        myQueue.add("B");
         myQueue.add("E");
+        myQueue.add("B");
         myQueue.add("G");
         myQueue.add("C");
         System.out.println(myQueue);
+        /*
+        Biz soylemedikce java priorty ile kendine gore bir siralama yapar ama biz istersek bazi siralamalar yapabiliriz
+         */
     }
 }
