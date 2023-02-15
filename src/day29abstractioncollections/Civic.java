733dbe06c41a9c08856a8b01b6fd303a3a1fd583
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 14:03:44 +0100

    Java

--- src/day29abstractioncollections/Civic.java
+++ src/day29abstractioncollections/Civic.java
@@ -4,6 +4,7 @@ public class Civic implements Ac,Motor{
 
     @Override
     public void run() {
-
+//interfacelerde methodbodysi olmadigi icin override ederken ikisinde ayni method olsa ile sikinti olmaz
+        System.out.println("Civic iyi calisir");
     }
 }
