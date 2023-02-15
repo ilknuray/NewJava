effdc4b3a3a1bfd56210ef55df8f960d77717f76
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 14:07:00 +0100

    Java

--- src/day29abstractioncollections/Civic.java
+++ src/day29abstractioncollections/Civic.java
@@ -1,10 +1,15 @@
 package day29abstractioncollections;
 
-public class Civic implements Ac,Motor{
+public class Civic implements Ac, Motor {
 
     @Override
     public void run() {
-//interfacelerde methodbodysi olmadigi icin override ederken ikisinde ayni method olsa ile sikinti olmaz
+        //interfacelerde methodbodysi olmadigi icin override ederken ikisinde ayni method olsa ile sikinti olmaz
+        //bodysi olan methodlarda detay verilir ancak abstract methodlarda yani method bodysi olmayn methodlar
+        // sadece ne yapacagini soyler nasil yapacagini soylemez(what to do==>soyler)
+        //concrate methodlar ise hem what to do hem how to do yu belirler
+        //interfaceler yapilacak isler listesidir(To do list)child classlara yapilacak isleri soylemis oluruz
+
         System.out.println("Civic iyi calisir");
     }
 }
