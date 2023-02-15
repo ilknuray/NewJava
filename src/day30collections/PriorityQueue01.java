8a5468c47f4d71c86656084a1a1e7f40e34b9357
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 22:04:23 +0100

    Java

---
+++ src/day30collections/PriorityQueue01.java
@@ -0,0 +1,15 @@
+package day30collections;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueue01 {
+    public static void main(String[] args) {
+        PriorityQueue <String>myQueue=new PriorityQueue<>();
+        myQueue.add("A");
+        myQueue.add("B");
+        myQueue.add("E");
+        myQueue.add("G");
+        myQueue.add("C");
+        System.out.println(myQueue);
+    }
+}
