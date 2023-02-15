b49057bd14cbe087c4d2431d894f254308a5f3c4
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 21:32:54 +0100

    Java

--- src/day30collections/TreeSet01.java
+++ src/day30collections/TreeSet01.java
@@ -1,6 +1,7 @@
 package day30collections;
 
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.TreeSet;
 
 public class TreeSet01 {
@@ -26,6 +27,18 @@ public class TreeSet01 {
         System.out.println("emails = " + emails);
         Long end1= LocalTime.now().toNanoOfDay();
         System.out.println(end1-start1);
+
+        HashSet<String>myEmails=new HashSet<>();
+        myEmails.add("a@gmail.com");
+        myEmails.add("b@gmail.com");
+        myEmails.add("c@gmail.com");
+        myEmails.add("d@gmail.com");
+        myEmails.add("e@gmail.com");
+        myEmails.add("f@gmail.com");
+        myEmails.add("g@gmail.com");
+        myEmails.add("h@gmail.com");
+        Long end2=LocalTime.now().toNanoOfDay();
+        System.out.println(end2-end1);
 
 
 
