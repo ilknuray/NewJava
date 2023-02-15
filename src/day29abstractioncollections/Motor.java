7aed2ef156b92a3275746a540148b3f0d3b642e5
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 14:53:38 +0100

    Java

--- src/day29abstractioncollections/Motor.java
+++ src/day29abstractioncollections/Motor.java
@@ -3,7 +3,7 @@ package day29abstractioncollections;
 public interface Motor extends Vehicle {
 
     /*
-    class==>interface=>imlements
+    class==>interface=>implements
     class==>==>class==>extends
     interface==>class==>mumkun degildir
     interface==>interace==>extends
@@ -14,5 +14,23 @@ public interface Motor extends Vehicle {
 
 
     double agirlik=20.8;
+
+    /*
+    abstract class ile interface arasindaki farklar nelerdir?
+    *methodlar
+    1)Abstract class hem abstract hemde oncrete methodlar icerebilir
+    interfaceler ise sadece abstract methodlar icerir.ancak istersek default ve static keywordu ile concrete metodlar olusturabiliriz
+
+    µµVariable
+    1)Abstract classlarda normal classlardaki gibi her turlu variable olusturabiliriz
+    interfacelerde ise variablelar public static ve final olmalidir
+
+    **Inheritance
+    1)abstract classlar class olduklari icin multiple inheritance soz konusu degildir
+    interfaceler ise multiple inheritance i destekler
+
+    **abstract classlarda constructor vardir ancak object olusturmakta kullanilamaz interfacelerde ise constructor olmadigindan object olusturulamaz
+
+     */
 
 }
