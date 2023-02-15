63deb00d4f2a5f4d15dcf3deea3599a7b47a4713
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 15:14:36 +0100

    Java

---
+++ src/day29abstractioncollections/Collections01.java
@@ -0,0 +1,12 @@
+package day29abstractioncollections;
+
+public class Collections01 {
+    //colection detince aklimiza list queue ve set gelmelidir
+    //colletonlar interface dir
+    //listeler iki ye ayrilir *arrylist **Linkedlist
+
+    //listlere ==>coklu ve tekrarli data depolamak istedigimizde ihtiyac duyariz ornegin okuldaki ogrenci listesi.
+    //setlere ==>coklu ama tekrarasiz data depolamak istersek ihtiyac duyariz ornegin ogrenci id.set ler unique degerleri tutarlar.
+    //Queue==>FIFO first in first out.LIFO last in last out gerektigin de kullanilir.bankada ki sira app i yapilirken kullanilir.sira manasindadir.
+
+}
---
+++ src/day29abstractioncollections/LinkedList01.java
@@ -0,0 +1,5 @@
+package day29abstractioncollections;
+
+public class LinkedList01 {
+
+}
