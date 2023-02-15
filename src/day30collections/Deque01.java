9a896416b2a5f89015c431be54ed45ecfd16c2f9
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 21:54:44 +0100

    Java

---
+++ src/day30collections/Deque01.java
@@ -0,0 +1,9 @@
+package day30collections;
+
+public class Deque01 {
+    public static void main(String[] args) {
+        /*
+        double ended queue demaktir.yani iki uclu queue demektir
+         */
+    }
+}
--- src/day30collections/Queue01.java
+++ src/day30collections/Queue01.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 
 public class Queue01 {
     public static void main(String[] args) {
-        Queue<String>depo=new LinkedList<>();
+        Queue<String> depo = new LinkedList<>();
         depo.add("milk");
         depo.add("bread");
         depo.add("honey");
@@ -15,7 +15,15 @@ public class Queue01 {
         //Queuelar elemanlari gelis sirasina gore dizerler
 
         depo.offer("Potatoes");//elemani eger kapasite uygunsa ekler cunku burada sinir koyabiliyorum.eger kapasite uygun degilse exception atar
-                           //boolean  doner
+        //boolean  doner
         System.out.println(depo);
+        /*
+        ilk giren elemani ilk kullanmamiz(firstin firstout)==>gerektiginde queue en iyi secimdir
+        Queue da elemanlar "insertion order" a gore dizilirler
+        Queue da peek(),pool(),element();remove() gibi ilk elemani ilgilendiren bir cok method vardir
+         offer() methodu kapasite kontrolu yapildiktan sonra eleman ekleme durumunda yapilir
+         */
+
+
     }
 }
