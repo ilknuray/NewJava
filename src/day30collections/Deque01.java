d46f3bb06d9b839fa1c7fc4fd578460d0dd72713
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-15 21:58:58 +0100

    Java

--- src/day30collections/Deque01.java
+++ src/day30collections/Deque01.java
@@ -1,9 +1,24 @@
 package day30collections;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Deque01 {
     public static void main(String[] args) {
         /*
         double ended queue demaktir.yani iki uclu queue demektir
+
          */
+        Deque<String>mobilyaKamyonu=new LinkedList<>();
+        mobilyaKamyonu.add("yatak");
+        mobilyaKamyonu.add("kanape");
+        mobilyaKamyonu.add("ayna");
+        mobilyaKamyonu.add("masa");
+        mobilyaKamyonu.add("sandalye");
+        mobilyaKamyonu.add("koltuk");
+        System.out.println("mobilyaKamyonu = " + mobilyaKamyonu);
+        //deque sadece linkedlistin constructorini kullanir
+
+
     }
 }
