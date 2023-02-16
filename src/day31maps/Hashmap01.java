5c475aca041670a7aa14d5f86c5fc0799f471fae
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-16 11:37:15 +0100

    Java

--- src/day31maps/Hashmap01.java
+++ src/day31maps/Hashmap01.java
@@ -10,10 +10,20 @@ public class Hashmap01 {
     3)Key kismi null olamaz,ama value kismi null olabilir
     4)Entry denir key-value ikilisine eleman yerine entry-set denir
     5)Eklenen datalari siralamak ile ilgilenmez super fast calisirlar
-
+    primitiveler hic bir collections ve maps te kullanilamaz
      */
+    public static void main(String[] args) {
 
-    HashMap<String,Integer> numaralar=new HashMap<>();
+
+        HashMap<String, Integer>studentsAge  = new HashMap<>();
+        studentsAge.put("Ali",13);
+        studentsAge.put("Tom",21);
+        studentsAge.put("Brad",12);
+        studentsAge.put("");
+        studentsAge.put("");
+        studentsAge.put("");
+        System.out.println(studentsAge);
 
 
+    }
 }
