eb38034499493451ac977613c5946335c8778d54
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-21 20:36:36 +0100

    Java

--- src/day31maps/HashMap02.java
+++ src/day31maps/HashMap02.java
@@ -10,7 +10,7 @@ public class HashMap02 {
         /*
         1)siz HashMap<String,Double>salaries=new HashMap<>(); olusturdugunuzda java memoryde 16 kutu iceren bir yapi olusturur
         ve bu yapidaki herbir kutuya index verir.indexler 0 dan 15 e kadardir
-        2)siz salaries.put() kodunu yazdiginizda java key icin bir hashcode olusturur bu hashcodu  15 E BOLER ve kalan sayiyi index olarak
+        2)siz salaries.put() kodunu yazdiginizda java key icin bir hashcode olusturur bu hashcodu  16 A BOLER ve kalan sayiyi index olarak
         belirler.bu elemani o indexe yerlestirir
         3)yerlestirirken 4 lu bir yapi olusturur bu yapinin ilk bolumune hashcodu ikinci bolume key i ucuncu bolume value yu dorduncu bolume
         pointeri koyar.bu cok bolumlu yapi linkedlistteki node yapisidir.Yani hashmap kutulari koydugu datalari linkedlist depolar
