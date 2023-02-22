d4824d015922ec527f9e712f8de58b31debfa7c1
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-22 11:42:37 +0100

    Java

--- src/day32maps/Maps02.java
+++ src/day32maps/Maps02.java
@@ -1,9 +1,24 @@
 package day32maps;
 
-public class Maps02 {
- //Size verilen bir kelimede kullanilan harflerin kac kere kullanildigini gosteren kodu yaainiz
- public static void main(String[] args) {
+import java.util.HashMap;
 
- }
+public class Maps02 {
+    //Size verilen bir kelimede kullanilan harflerin kac kere kullanildigini gosteren kodu yaainiz
+    public static void main(String[] args) {
+        String str = "abbcaa";
+        String[] harfler = str.split("");
+
+        HashMap<String, Integer> kacKere = new HashMap<>();
+        for (String w : harfler) {
+            Integer harfSayisi = kacKere.get(w);
+            if (harfSayisi == null) {
+                kacKere.put(w, 1);
+            } else {
+                kacKere.put(w, harfSayisi + 1);
+            }
+
+        }
+        System.out.println(kacKere);
+    }
 
 }
