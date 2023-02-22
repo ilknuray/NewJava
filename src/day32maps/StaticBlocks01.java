6607f46683d5b3b6b1c54692a9b753d84540f84d
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-22 12:18:28 +0100

    Java

--- src/day32maps/StaticBlocks01.java
+++ src/day32maps/StaticBlocks01.java
@@ -3,8 +3,20 @@ package day32maps;
 ;
 
 public class StaticBlocks01 {
+    public static double pi;
+
+    static {
+        pi=3.14;
+        /*
+        Statik blocklar ihtiyacimiz olan variablelain class olusturma safhasinda elimizde olmasini saglar
+        statik blocklar class icindeki herseyden once calistirilirlar
+        static blocklaricerisinde sadece static variablelara deger atanabilir.
+         */
+        System.out.println("Static block1");
+    }
 
     public static void main(String[] args) {
+        System.out.println("main method");
 
 
     }
