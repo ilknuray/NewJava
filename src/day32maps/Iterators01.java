035e1e2bc9d383e96f7eedd1a040fc1d678da831
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-22 12:31:49 +0100

    Java

---
+++ src/day32maps/Iterators01.java
@@ -0,0 +1,18 @@
+package day32maps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Iterators01 {
+    public static void main(String[] args) {
+        List<String>myList=new ArrayList<>();
+        myList.add("Z");
+        myList.add("K");
+        myList.add("A");
+        myList.add("J");
+        myList.add("M");
+        System.out.println("myList = " + myList);
+        //foreach kullanarak her elemanin sonuna ! koyunuz.
+
+    }
+}
