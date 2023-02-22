323a0ee554de5afe2169472c907b11bd05b7e5a4
ilknur yilmaz <ilknuray1318@gmail.com>
2023-02-22 13:34:46 +0100

    Java

--- src/day32maps/USstates.java
+++ src/day32maps/USstates.java
@@ -2,5 +2,22 @@ package day32maps;
 
 public enum USstates {
 
+    ALABAMA("alabama", "al"),
+    ALASKA("alaska","ak");
 
+    private final String stateName;
+    private final  String kisaltma;
+
+    USstates(String stateName, String kisaltma) {
+        this.stateName = stateName;
+        this.kisaltma = kisaltma;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public String getKisaltma() {
+        return kisaltma;
+    }
 }
